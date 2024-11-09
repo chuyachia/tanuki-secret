c62eb3688f152d400923212608a29261fda1c43b
Chu-Ya Chia <chuyachia@gmail.com>
2024-11-08 20:25:03 -0500

    Add different move and rotation mode, fix climb

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,4 +1,4 @@
-using UnityEditor.Callbacks;
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -15,12 +15,15 @@ public class CharacterControl : MonoBehaviour
     public float AirAcceleration = 2f;
     public float TurnSpeed = 35f;
     public float JumpForce = 5f;
-    public float DecelerateForce = 5f;
     public float MaxGroundAngle = 25f;
+    public float MinClimbaleAngle = 60f;
     public float ClimbSurfaceRayCastDistance = 10f;
     public LayerMask ClimbableSurface;
+    public float ModelHeight = 2.5f;
 
     public float AdditionalAccelerationOnSlope = 2f;
+    public MoveType moveType;
+    public RotateType rotateType;
 
 
     private Rigidbody _rb;
@@ -29,18 +32,32 @@ public class CharacterControl : MonoBehaviour
     private bool _onSlope;
     private bool _hasClimbSurfaceContact;
     private float _minGroundDotProduct;
-    private Vector3 _contactNormal = Vector3.up;
-    private Vector3 _climbNormal = Vector3.up;
+    private float _maxClimbDotProduct;
+    private Vector3 _contactNormal = Vector3.zero;
+    private Vector3 _climbDir = Vector3.zero;
     private float _inputHorizontal;
     private float _inputVertical;
     private bool _shouldClimb;
     private bool _shouldJump;
     private bool _isClimbing;
 
+    public enum MoveType
+    {
+        FromPlayerPerspective,
+        InWordSpace
+    }
+
+    public enum RotateType
+    {
+        Continuous,
+        Discrete
+    }
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _minGroundDotProduct = Mathf.Cos(MaxGroundAngle * Mathf.Deg2Rad);
+        _maxClimbDotProduct = Mathf.Cos(MinClimbaleAngle * Mathf.Deg2Rad);
     }
 
     void Update()
@@ -49,7 +66,6 @@ public class CharacterControl : MonoBehaviour
         _inputVertical = Input.GetAxis("Vertical");
         _shouldClimb = Input.GetKeyDown(KeyCode.E);
         _shouldJump = Input.GetKeyDown(KeyCode.Space);
-        CheckClimbState();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -65,16 +81,13 @@ public class CharacterControl : MonoBehaviour
     void EvaluateGroundCollision(Collision collision)
     {
         int contactNormalCount = 0;
-        int climbNormalCount = 0;
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
             float upDot = Vector3.Dot(Vector3.up, normal);
-            if ((ClimbableSurface.value & (1 << collision.transform.gameObject.layer)) > 0)
+            if (upDot < _maxClimbDotProduct && (ClimbableSurface.value & (1 << collision.transform.gameObject.layer)) > 0)
             {
                 _hasClimbSurfaceContact = true;
-                climbNormalCount++;
-                _climbNormal += normal;
             }
             else
             {
@@ -89,10 +102,6 @@ public class CharacterControl : MonoBehaviour
                 contactNormalCount++;
                 _contactNormal += normal;
             }
-        }
-        if (climbNormalCount > 1)
-        {
-            _climbNormal.Normalize();
         }
         if (contactNormalCount > 1)
         {
@@ -127,52 +136,88 @@ public class CharacterControl : MonoBehaviour
         return Acceleration;
     }
 
-    void Rotate()
+    void ContinuousRotate()
     {
         float rotationAmount = 0;
         if (_inputHorizontal != 0)
         {
             rotationAmount = _inputHorizontal * TurnSpeed;
         }
-        Quaternion alignToGround = Quaternion.FromToRotation(Vector3.up, _contactNormal);
-        Quaternion targetRotation = Quaternion.Euler(alignToGround.eulerAngles.x, _rb.rotation.eulerAngles.y + rotationAmount, alignToGround.eulerAngles.x);
-        _rb.MoveRotation(Quaternion.RotateTowards(_rb.rotation, targetRotation, TurnSpeed * Time.fixedDeltaTime));
+        // Align to ground
+        // Quaternion alignToGround = Quaternion.FromToRotation(transform.up, _contactNormal);
+        // Quaternion targetRotation = Quaternion.Euler(alignToGround.eulerAngles.x, _rb.rotation.eulerAngles.y + rotationAmount, alignToGround.eulerAngles.x);
+
+        Quaternion targetRotation = Quaternion.Euler(_rb.rotation.eulerAngles.x, _rb.rotation.eulerAngles.y + rotationAmount, _rb.rotation.eulerAngles.z);
+        _rb.rotation = targetRotation;
     }
 
-    void Move()
+    void MoveInWordSpace()
     {
-        float targetSpeed = _isClimbing ? ClimbSpeed : MoveSpeed;
-        if (_rb.velocity.magnitude >= targetSpeed)
+        Vector3 xAxis = ProjectOnContactPlane(Vector3.right, _contactNormal).normalized;
+        Vector3 zAxis = ProjectOnContactPlane(Vector3.forward, _contactNormal).normalized;
+
+        float currentX = Vector3.Dot(_rb.velocity, xAxis);
+        float currentZ = Vector3.Dot(_rb.velocity, zAxis);
+
+        float acceleration = GetAcceleration();
+        float maxSpeedChange = acceleration * Time.fixedDeltaTime;
+        Vector3 inputMovement = new Vector3(_inputHorizontal, 0f, _inputVertical);
+        if (inputMovement == Vector3.zero)
         {
             return;
         }
-        if (_isClimbing)
+
+        Vector3 targetVelocity = inputMovement * MoveSpeed;
+        float newX = Mathf.MoveTowards(currentX, targetVelocity.x, maxSpeedChange);
+        float newZ = Mathf.MoveTowards(currentZ, targetVelocity.z, maxSpeedChange);
+        Vector3 currentVelocity = xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+        _rb.velocity += currentVelocity;
+    }
+
+    void DiscreteRotate()
+    {
+        if (_inputVertical > 0)
         {
-            _rb.velocity = new Vector3(_rb.velocity.x, _rb.velocity.y + _inputVertical * ClimbSpeed * Time.fixedDeltaTime, _rb.velocity.z);
+            _rb.rotation = Quaternion.LookRotation(Vector3.forward);
         }
-        else
+        else if (_inputVertical < 0)
         {
-            if (_inputVertical != 0)
-            {
-                Vector3 adjustedForwardDir = ProjectOnContactPlane(transform.forward, _contactNormal).normalized;
-                float currentMoveSpeed = Vector3.Dot(_rb.velocity, adjustedForwardDir);
-                float desiredMoveSpeed = _inputVertical * MoveSpeed;
-                float acceleration = GetAcceleration();
-                float maxSpeedChange = acceleration * Time.fixedDeltaTime;
-                float newMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, desiredMoveSpeed, maxSpeedChange);
-                _rb.velocity += adjustedForwardDir * (newMoveSpeed - currentMoveSpeed);
-            }
-            else
-            {
-                _rb.AddForce(-_rb.velocity.normalized * DecelerateForce, ForceMode.Acceleration);
-            }
+            _rb.rotation = Quaternion.LookRotation(Vector3.back);
         }
+        if (_inputHorizontal > 0)
+        {
+            _rb.rotation = Quaternion.LookRotation(Vector3.right);
+        }
+        else if (_inputHorizontal < 0)
+        {
+            _rb.rotation = Quaternion.LookRotation(Vector3.left);
+        }
+    }
+
+    void MoveFromPlayerPerspective()
+    {
+        if (_inputVertical != 0)
+        {
+            Vector3 adjustedForwardDir = ProjectOnContactPlane(transform.forward, _contactNormal).normalized;
+            float currentMoveSpeed = Vector3.Dot(_rb.velocity, adjustedForwardDir);
+            float desiredMoveSpeed = _inputVertical * MoveSpeed;
+            float acceleration = GetAcceleration();
+            float maxSpeedChange = acceleration * Time.fixedDeltaTime;
+            float newMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, desiredMoveSpeed, maxSpeedChange);
+            _rb.velocity += adjustedForwardDir * (newMoveSpeed - currentMoveSpeed);
+        }
+    }
+
+
+    void Climb()
+    {
+        _rb.velocity += _climbDir * ClimbSpeed * _inputVertical * Time.fixedDeltaTime;
     }
 
     void ClearState()
     {
         _onSlope = _onGround = _hasClimbSurfaceContact = false;
-        _contactNormal = Vector3.up;
+        _contactNormal = Vector3.zero;
     }
 
     void SnapToSurface()
@@ -187,9 +232,10 @@ public class CharacterControl : MonoBehaviour
 
     void CheckClimbState()
     {
-        if (_shouldClimb && _hasClimbSurfaceContact)
+        if (_shouldClimb &&
+        _hasClimbSurfaceContact &&
+         Physics.Raycast(transform.position, transform.forward, ClimbSurfaceRayCastDistance, ClimbableSurface))
         {
-            Debug.Log("Start climbing");
             _isClimbing = true;
             return;
         }
@@ -201,13 +247,19 @@ public class CharacterControl : MonoBehaviour
         if (_shouldJump)
         {
             _isClimbing = false;
+            _climbDir = Vector3.zero;
             Debug.Log("Stop climbing");
         }
         else
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, -_climbNormal, out hit, ClimbSurfaceRayCastDistance, ClimbableSurface))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, ClimbSurfaceRayCastDistance, ClimbableSurface))
             {
+                Debug.DrawRay(transform.position, transform.forward, Color.red, 5f);
+                Debug.DrawRay(hit.point, hit.normal, Color.blue, 5f);
+                _climbDir = Vector3.Cross(hit.normal, -transform.right).normalized; // only works when facing climb object
+                Debug.DrawRay(transform.position, _climbDir, Color.black, 5f);
+
                 Debug.Log("Still climbing");
                 _isClimbing = true;
             }
@@ -215,14 +267,52 @@ public class CharacterControl : MonoBehaviour
             {
                 Debug.Log("Fell from climbing");
                 _isClimbing = false;
+                _climbDir = Vector3.zero;
             }
         }
     }
 
+    bool WithinSpeedLimit()
+    {
+        float targetSpeed = _isClimbing ? ClimbSpeed : MoveSpeed;
+        if (_rb.velocity.magnitude >= targetSpeed)
+        {
+            return false;
+        }
+        return true;
+    }
+
     void FixedUpdate()
     {
-        Rotate();
-        Move();
+        CheckClimbState();
+        if (WithinSpeedLimit())
+        {
+            if (_isClimbing)
+            {
+                Climb();
+            }
+            else
+            {
+                switch (moveType)
+                {
+                    case MoveType.FromPlayerPerspective:
+                        MoveFromPlayerPerspective();
+                        break;
+                    case MoveType.InWordSpace:
+                        MoveInWordSpace();
+                        break;
+                }
+            }
+        }
+        switch (rotateType)
+        {
+            case RotateType.Continuous:
+                ContinuousRotate();
+                break;
+            case RotateType.Discrete:
+                DiscreteRotate();
+                break;
+        }
         SnapToSurface();
         Jump();
         ClearState();
